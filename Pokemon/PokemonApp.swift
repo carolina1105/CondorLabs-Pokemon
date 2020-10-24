ca675803fcaab199e06900d4de5e13c5a9dd005f
Laddy Carolina Diaz <carolina.diaz@mdcloudps.com>
2020-10-24 09:13:53 -0500

    initial commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Laddy Diaz Lamus on 24/10/20.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
